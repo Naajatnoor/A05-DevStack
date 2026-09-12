2bcdd177d5578a5e7cc3c23bbc99cbdfbb02c96f
Naajatnoor <295597711+Naajatnoor@users.noreply.github.com>
2026-09-13 00:50:14 +0600

    create technology heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Nav from "./components/Nav"
 import Banner from "./components/Banner"
 import Footer from "./components/Footer"
+import Technology from "./components/Technology"
 
 function App() {
   return (
     <>
       <Nav></Nav>
       <Banner></Banner>
+      <Technology></Technology>
       <Footer></Footer>
+
     </>
   )
 }
