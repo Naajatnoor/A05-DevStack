b00affb439ed161f45bb6f817608dd8dbdb6ab1a
Naajatnoor <295597711+Naajatnoor@users.noreply.github.com>
2026-09-12 21:22:09 +0600

    done with  Navbar.created navbar componen,style navbar layout and navigation links.Experiment with responsive nevbar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Logo from '../assets/logo-text.png'
+import "./Nav.css";
+const Nav = () => {
+    return (
+        <nav className='flex justify-between items-center mx-25 my-5'>
+          <button className="md:hidden text-2xl">
+  ☰
+</button>
+            <img src={Logo} alt="" />
+            <ul  className=' hidden md:flex justify-between items-center gap-6 ' >
+                <li><a href="#"><span className="text-[#DB2777]">Home</span></a></li>
+                <li><a href="#">Technologies</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div className=' flex gap-2.5'>
+               <button className="btn rounded-full bg-white px-6 text-[#334155]">Sign in</button>
+               <button className="btn rounded-full bg-[#D91B7E] text-white px-6 hover:border-gray-400 ">Sign Up</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
